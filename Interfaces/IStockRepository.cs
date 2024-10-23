0ee8e64200b8cc7ea2e9c7e296e5eacd21c23239
sheel-ui <sheeltaskar64@gmail.com>
2024-10-22 22:02:42 -0400

    add comment controller

--- Interfaces/IStockRepository.cs
+++ Interfaces/IStockRepository.cs
@@ -13,5 +13,6 @@ namespace api.Interfaces
         Task<Stock> CreateAsync(Stock stockModel);
         Task<Stock?> UpdateAsync(int id, UpdateStockDto stockDto);
         Task<Stock?> DeleteAsync(int id);
+        Task<bool> StockExists(int id);
     }
 }
--- Repository/StockRepository.cs
+++ Repository/StockRepository.cs
@@ -41,12 +41,12 @@ namespace api.Repository
 
         public async Task<List<Stock>> GetAllAsync()
         {
-            return await _context.Stocks.ToListAsync();
+            return await _context.Stocks.Include(c => c.Comments).ToListAsync();
         }
 
         public async Task<Stock?> GetByIdAsync(int id)
         {
-            return await _context.Stocks.FindAsync(id);
+            return await _context.Stocks.Include(c => c.Comments).FirstOrDefaultAsync(i => i.Id == id);
         }
 
         public async Task<Stock?> UpdateAsync(int id, UpdateStockDto stockDto)
@@ -68,6 +68,10 @@ namespace api.Repository
             await _context.SaveChangesAsync();
 
             return existingStock;
+        }
+        public Task<bool> StockExists(int id)
+        {
+            return _context.Stocks.AnyAsync(s => s.Id == id);
         }
     }
 }
